f0adfa6f0ec119cc601b8fad8502943a50e5b123
Surjith S M <surjithctly@gmail.com>
2023-07-28 21:08:46 +0530

    feature grid and testimonials

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,19 @@ module.exports = {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
       },
+      animation: {
+        marquee: "marquee 50s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          from: {
+            transform: "translateX(0)",
+          },
+          to: {
+            transform: "translateX(calc(-100% - 2.5rem))",
+          },
+        },
+      },
     },
   },
   plugins: [require("@tailwindcss/typography")],
