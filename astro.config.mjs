b9f7a3b6c419a9b7f9587a7a7fa5e1d335f24f0e
Surjith S M <surjithctly@gmail.com>
2023-08-03 21:15:11 +0530

    features

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,16 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, sharpImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://astroship-pro.web3templates.com",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-    sitemap(),
-  ],
+  experimental: {
+    assets: true,
+    viewTransitions: true,
+  },
+  image: {
+    service: sharpImageService(),
+  },
+  integrations: [tailwind(), mdx(), sitemap()],
 });
